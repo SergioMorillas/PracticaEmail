8fd689329ea4c25b3d989be4a409399cb017e967
Sergio <sergiomc11756@gmail.com>
2024-12-03 17:50:52 +0100

    Practica del dia 3/12/2024

--- prueba.php
+++ prueba.php
@@ -3,48 +3,48 @@
 require "PHPMailer/src/PHPMailer.php";
 require "PHPMailer/src/SMTP.php";
 require "PHPMailer/src/Exception.php";
-use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
 include_once "cifrarDescifrar.php";
 
-
-function mandarCorreo() {
+function mandarCorreo($correo, $cc, $bcc, $asunto, $cuerpo, $adjunto)
+{
     $mail = new PHPMailer(true);
 
-$mail->SMTPDebug = 0;
-$mail->isSMTP();
-$mail->Host       = "smtp.gmail.com";
-$mail->SMTPAuth   = true;
-$mail->Username   = "sergiomc11756@gmail.com";
-$mail->Password   = getPass();
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-$mail->Port       = 465;
-$mail->CharSet    = "UTF-8";
+    $mail->SMTPDebug = 0;
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "sergiomc11756@gmail.com";
+    $mail->Password = getPass();
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 465;
+    $mail->CharSet = "UTF-8";
 
-$to = "danielalonsodaw@gmail.com"; 
+    $to = "danielalonsodaw@gmail.com";
 
-// $mail->setFrom("sergiomorillas02@gmail.com", "Sergio Morillas");
-if (!empty($to)) {
-    $mail->addAddress($to);
-} else {
-    echo "La dirección de correo del destinatario no está definida. ";
-}
+    if (!empty($to)) {
+        $mail->addAddress($to);
+    } else {
+        echo "La dirección de correo del destinatario no está definida. ";
+    }
 
 // Contenido
-$mail->addcc("sergiomc11756@gmail.com", "sergio");
-$mail->addbcc("sergiomorillas02@gmail.com", "sergio");
-$mail->addAttachment("imagen.png");
-$mail->Subject = "MI ASUNTO PERSONALIZADO";
-$mail->Body    = "EL CONTENIDO DE MI CORREO UTILIZANDO PHPMAILERsergio en bccsergio en bccsergio en bccsergio en bcc";
+    $mail->addcc("sergiomc11756@gmail.com", "sergio");
+    $mail->addbcc("sergiomorillas02@gmail.com", "sergio");
+    $mail->addAttachment("imagen.png");
+    $mail->Subject = "MI ASUNTO PERSONALIZADO";
+    $mail->Body = "EL CONTENIDO DE MI CORREO UTILIZANDO PHPMAILERsergio en bccsergio en bccsergio en bccsergio en bcc";
 
-try {
-    $mail->send();
-    echo "El mensaje ha sido enviado";
-} catch (Exception $e) {
-    echo "El mensaje no se pudo enviar. Mailer Error: {$mail->ErrorInfo}";
+    try {
+        $mail->send();
+        echo "El mensaje ha sido enviado";
+    } catch (Exception $e) {
+        echo "El mensaje no se pudo enviar. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
-}
-function getPass() {
+function getPass()
+{
     $file = fopen(".env", "r");
     $cifrada = trim(explode("@", fgets($file))[1]);
     $clave = trim(explode("@", fgets($file))[1]);
@@ -54,5 +54,3 @@ function getPass() {
     return descifrar($cifrada, $clave);
 }
 echo "\nContraseña: " . getPass();
-
-?>
